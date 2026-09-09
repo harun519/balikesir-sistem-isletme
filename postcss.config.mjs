8d1e1c22ba54401028dd831709695138da295740
harun519 <harun519@gmail.com>
2026-09-09 09:16:23 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,7 @@
-export default { plugins: { "@tailwindcss/postcss": {} } };
+import tailwindcss from "@tailwindcss/postcss";
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
